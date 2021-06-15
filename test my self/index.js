93dbcb449de3d1e4476447f9275761746965a800
ppstars <pp@pp.pp>
2021-06-15 23:32:43 +0800

    新增 csdn blog

--- test my self/index.js
+++ test my self/index.js
@@ -5,20 +5,24 @@ const { exitOnError } = require('winston');
 const { JSDOM } = require('jsdom');
 
 (async () => {
-    const host = 'https://gouhuo.qq.com/content/tablist/1_110';
+    const host = 'https://blog.csdn.net/weixin_39430411';
     // 发起 HTTP GET 请求
     const response = await got({
         method: 'get',
         url: host,
     });
-    // const data = response.body;
-    const data =  new JSDOM(response.body, {
+    const data = response.body;
+    var data2 = data.replace('window.csdn.sideToolbar = ', '')
+    data2 =  new JSDOM(data2, {
         runScripts: 'dangerously',
     });
 
-    const list = data.window.__NUXT__.data[0].list;
-    // console.log(data2);
-    // console.log(data2.length);
+    const blogTitle = data2.window.__INITIAL_STATE__.pageData.data.baseInfo.blogModule.title;
+    console.log(blogTitle);
+
+    const list = data2.window.__INITIAL_STATE__.pageData.data.baseInfo.latelyList;
+    // console.log(response.body);
+    // console.log(list);
     // const $ = cheerio.load(data);
 
     // const list = $('.b-list__row.b-list-item.b-imglist-item .b-list__main .b-list__tile p.b-list__main__title').get();
@@ -28,7 +32,7 @@ const { JSDOM } = require('jsdom');
     const items = list.map((i) => {
         // const item = $(i);
         // console.log(i, new Date().toUTCString());
-        const url = i.link;
+        const url = i.url;
         // console.log(url);
         const title = i.title;
         // console.log(url);
@@ -36,58 +40,58 @@ const { JSDOM } = require('jsdom');
 
         const single = {
             title: title,
-            description: title,
+            description: i.description,
             link: url,
-            pubDate: new Date().toUTCString(),
         };
         return single;
     });
+    console.log(items)
     // console.log(items);
 
-    const result = await Promise.all(
-        items.map(async (item) => {
-            // console.log(item.link);
-            // const cache = await ctx.cache.get(link);
-            // if (cache) {
-            //     return Promise.resolve(JSON.parse(cache));
-            // }
+    // const result = await Promise.all(
+    //     items.map(async (item) => {
+    //         // console.log(item.link);
+    //         // const cache = await ctx.cache.get(link);
+    //         // if (cache) {
+    //         //     return Promise.resolve(JSON.parse(cache));
+    //         // }
 
-            const response = await got({
-                method: 'get',
-                url: item.link,
-            });
-            // const data = response.body;
-            const data =  new JSDOM(response.body, {
-                runScripts: 'dangerously',
-            });
+    //         const response = await got({
+    //             method: 'get',
+    //             url: item.link,
+    //         });
+    //         // const data = response.body;
+    //         const data =  new JSDOM(response.body, {
+    //             runScripts: 'dangerously',
+    //         });
 
-            const c = data.window.__NUXT__.data[0];
+    //         const c = data.window.__NUXT__.data[0];
 
-            // console.log(c.richText);
+    //         // console.log(c.richText);
 
-            // const itemReponse = await got.get(item.link);
+    //         // const itemReponse = await got.get(item.link);
 
-            // const data = itemReponse.body;
+    //         // const data = itemReponse.body;
 
-            // const itemElement = cheerio.load(data);
+    //         // const itemElement = cheerio.load(data);
 
-            // const t = itemElement('.article-info > .date').text()
-            // console.log(t)
-            // t = Date.parse(t)
-            // console.log(Date.parse(t))
-            // console.log(new Date(t * 1000).toUTCString());
+    //         // const t = itemElement('.article-info > .date').text()
+    //         // console.log(t)
+    //         // t = Date.parse(t)
+    //         // console.log(Date.parse(t))
+    //         // console.log(new Date(t * 1000).toUTCString());
 
-            // var description = itemElement('.c-post__body .c-article__content').html();
-            item.description = c.richText;
-            // return item;
-            // dd = JSON.parse(dd);
-            // console.log(description);
+    //         // var description = itemElement('.c-post__body .c-article__content').html();
+    //         item.description = c.richText;
+    //         // return item;
+    //         // dd = JSON.parse(dd);
+    //         // console.log(description);
 
-            // console.log(item.description);
-            // ctx.cache.set(link, JSON.stringify(item));
-            return Promise.resolve(item);
-        })
-    );
+    //         // console.log(item.description);
+    //         // ctx.cache.set(link, JSON.stringify(item));
+    //         return Promise.resolve(item);
+    //     })
+    // );
 
     // console.log(result[0]);
 
